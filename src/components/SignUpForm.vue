f5d1ac82c74452e249c4adad31a8aa0708017c23
ort01 <ortutova.kristina@gmail.com>
2023-01-04 16:03:59 +0100

    two way data-binding

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required v-model="email" />
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
+  </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+</template>
+
+<script>
+export default {
+  name: "SignUpForm",
+  props: [""],
+  data: () => {
+    return {
+      email: "mario",
+      password: "",
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 25px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input:focus {
+  outline: none;
+}
+</style>
